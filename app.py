b13d3edc53c472297feefc97032cd9353df73118
Egor <esemevskiy@gmail.com>
2020-02-10 00:15:03 +0300

    flask finished project

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from _datetime import datetime
+
 
 app = Flask(__name__, static_folder='./static')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
@@ -19,32 +20,38 @@ class BlogPost(db.Model):
         return f'Blog Post {str(self.id)}'
 
 
-all_posts = [
-    {
-        'title': 'Post 1',
-        'content': 'This is content of post 1',
-        'author': 'Egor',
-    },
-    {
-        'title': 'Post 2',
-        'content': 'This is content of post 2',
-        'author': 'Egor',
-    },
-    {
-        'title': 'Post 3',
-        'content': 'This is content of post 3',
-    }
-]
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/posts')
+@app.route('/posts', methods=['GET', 'POST'])
 def posts():
-    return render_template('posts.html', posts=all_posts)
+    if request.method == 'POST':
+        post_title = request.form['title']
+        post_content = request.form['content']
+        post_author = request.form['author']
+        new_post = BlogPost(title=post_title, content=post_content, author=post_author)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        all_posts = BlogPost.query.order_by(BlogPost.data_posted).all()
+        return render_template('posts.html', posts=all_posts)
+
+
+@app.route('/posts/new', methods=['GET', "POST"])
+def new_post():
+    if request.method == 'POST':
+        post_title = request.form['title']
+        post_content = request.form['content']
+        post_author = request.form['author']
+        new_post = BlogPost(title=post_title, content=post_content, author=post_author)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template('new.html')
 
 
 @app.route('/home/<string:name>/posts/<int:id>')
@@ -55,6 +62,27 @@ def hello(name, id):
 @app.route('/onlyget', methods=['GET'])
 def get_req():
     return 'you can only get this webpage'
+
+
+@app.route('/posts/edit/<int:id>', methods=["GET", "POST"])
+def edit(id):
+    post = BlogPost.query.get_or_404(id)
+    if request.method == "POST":
+        post.title = request.form['title']
+        post.author = request.form['author']
+        post.content = request.form['content']
+        db.commit()
+        return redirect('/posts')
+    else:
+        return render_template('edit.html', post=post)
+
+
+@app.route('/posts/delete/<int:id>')
+def delete_post(id):
+    post = BlogPost.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/posts')
 
 
 if __name__ == '__main__':
